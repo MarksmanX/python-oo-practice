e9cccc75edceacb2458caa7890ab9991af404839
MarksmanX <mkeph561@gmail.com>
2024-06-11 12:35:01 -0400

    Finished Project

--- serial.py
+++ serial.py
@@ -19,4 +19,25 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start=0):
+        """Make a new generator, starting at start."""
 
+        self.start = start
+        self.next = start
+        
+    def __repr__(self):
+        """Show representation."""
+
+        return f"<SerialGenerator start={self.start} next={self.next}>"
+
+    def generate(self):
+        """This will generate numbers beginning at the start and incrementing by 1 each time generate is called"""
+
+        self.next += 1
+        return self.next - 1
+    
+    def reset(self):
+        """Resets the number to the start number"""
+
+        self.next = self.start
+        
